12ad361735c6cb613e318cfc75a995fa2d559d17
Rodrigo Fernandes Bulgarelli <rodrigo196@gmail.com>
2017-05-25 10:53:05 -0300

    Rework

--- app/src/main/java/br/com/bulgasoftwares/feedreader/extensions/StringExtensions.kt
+++ app/src/main/java/br/com/bulgasoftwares/feedreader/extensions/StringExtensions.kt
@@ -1,6 +1,31 @@
 package br.com.bulgasoftwares.feedreader.extensions
 
-val String.md5 : String
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+val String.md5: String
     get() {
-      return Hash.md5(this)
+        return md5(this) ?:
+                throw IllegalStateException("Unable to compute md5 hash for string $this")
     }
+
+private fun md5(`in`: String): String? {
+    val digest: MessageDigest
+    try {
+        digest = MessageDigest.getInstance("MD5")
+        digest.reset()
+        digest.update(`in`.toByteArray())
+        val a = digest.digest()
+        val len = a.size
+        val sb = StringBuilder(len shl 1)
+        for (anA in a) {
+            sb.append(Character.forDigit(anA.toInt() and 0xf0 shr 4, 16))
+            sb.append(Character.forDigit(anA.toInt() and 0x0f, 16))
+        }
+        return sb.toString()
+    } catch (e: NoSuchAlgorithmException) {
+        e.printStackTrace()
+    }
+
+    return null
+}
